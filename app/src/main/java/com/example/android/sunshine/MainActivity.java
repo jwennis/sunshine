2b1e1e6b621dd30ac7248730718ef25277462992
Joe <j.w.ennis@gmail.com>
2017-01-11 16:13:26 -0500

    Create AsyncTask in MainActivity

--- app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.android.sunshine;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.example.android.sunshine.data.SunshinePrefs;
 
+
 public class MainActivity extends AppCompatActivity {
 
     private TextView mWeatherText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +23,31 @@ public class MainActivity extends AppCompatActivity {
         mWeatherText = (TextView) findViewById(R.id.weather_data);
 
         String location = SunshinePrefs.getPreferredLocation(this);
+
+        new FetchWeatherTask().execute(location);
+    }
+
+
+    public class FetchWeatherTask extends AsyncTask<String, Void, String[]> {
+
+
+        @Override
+        protected String[] doInBackground(String... params) {
+
+            if (params.length == 0) {
+
+                return null;
+            }
+
+            String location = params[0];
+
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(String[] result) {
+
+        }
     }
 }
