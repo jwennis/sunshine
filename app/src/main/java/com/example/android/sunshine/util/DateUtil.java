7aa39efc8f9acdce1b2fd5d07166ce1f9bf255f4
Joe <j.w.ennis@gmail.com>
2017-01-11 16:22:30 -0500

    Create DateUtil

---
+++ app/src/main/java/com/example/android/sunshine/util/DateUtil.java
@@ -0,0 +1,112 @@
+package com.example.android.sunshine.util;
+
+import android.content.Context;
+import android.text.format.DateUtils;
+
+import com.example.android.sunshine.R;
+
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
+public class DateUtil {
+
+    public static final long SECOND_MS = 1000;
+    public static final long MINUTE_MS = SECOND_MS * 60;
+    public static final long HOUR_MS = MINUTE_MS * 60;
+    public static final long DAY_MS = HOUR_MS * 24;
+
+
+    public static long getUTCDateFromLocal(long localDate) {
+
+        return localDate + getGmtOffset(localDate);
+    }
+
+
+    public static long getLocalDateFromUTC(long utcDate) {
+
+        return utcDate - getGmtOffset(utcDate);
+    }
+
+
+    private static long getGmtOffset(long date) {
+
+        return TimeZone.getDefault().getOffset(date);
+    }
+
+
+    public static long normalizeDate(long date) {
+
+        return date / DAY_MS * DAY_MS;
+    }
+
+
+    public static long getDayNumber(long date) {
+
+        return (date + getGmtOffset(date)) / DAY_MS;
+    }
+
+
+    private static String getDayName(Context context, long dateMs) {
+
+        long dayNumber = getDayNumber(dateMs);
+        long currentDayNumber = getDayNumber(System.currentTimeMillis());
+
+        if (dayNumber == currentDayNumber) {
+
+            return context.getString(R.string.today);
+
+        } else if (dayNumber == currentDayNumber + 1) {
+
+            return context.getString(R.string.tomorrow);
+
+        } else {
+
+            return new SimpleDateFormat("EEEE").format(dateMs);
+        }
+    }
+
+
+    private static String getReadableDateString(Context context, long timeMs) {
+
+        int flags = DateUtils.FORMAT_SHOW_DATE
+                | DateUtils.FORMAT_NO_YEAR | DateUtils.FORMAT_SHOW_WEEKDAY;
+
+        return DateUtils.formatDateTime(context, timeMs, flags);
+    }
+
+
+    public static String getFriendlyDateString(Context context, long dateMs, boolean showFullDate) {
+
+        long localDate = getLocalDateFromUTC(dateMs);
+        long dayNumber = getDayNumber(localDate);
+        long currentDayNumber = getDayNumber(System.currentTimeMillis());
+
+        if (dayNumber == currentDayNumber || showFullDate) {
+
+            String dayName = getDayName(context, localDate);
+            String readableDate = getReadableDateString(context, localDate);
+
+            if (dayNumber - currentDayNumber < 2) {
+
+                String localizedDayName = new SimpleDateFormat("EEEE").format(localDate);
+
+                return readableDate.replace(localizedDayName, dayName);
+
+            } else {
+
+                return readableDate;
+            }
+
+        } else if (dayNumber < currentDayNumber + 7) {
+
+            return getDayName(context, localDate);
+
+        } else {
+
+            int flags = DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_NO_YEAR
+                    | DateUtils.FORMAT_ABBREV_ALL | DateUtils.FORMAT_SHOW_WEEKDAY;
+
+            return DateUtils.formatDateTime(context, localDate, flags);
+        }
+    }
+}
--- app/src/main/java/com/example/android/sunshine/util/JsonUtil.java
+++ app/src/main/java/com/example/android/sunshine/util/JsonUtil.java
@@ -55,8 +55,35 @@ public class JsonUtil {
         parsedWeatherData = new String[weatherArray.length()];
 
         long localDate = System.currentTimeMillis();
+        long utcDate = DateUtil.getUTCDateFromLocal(localDate);
+        long startDay = DateUtil.normalizeDate(utcDate);
 
-        //...
+        for (int i = 0; i < weatherArray.length(); i++) {
+
+            String date;
+            String highAndLow;
+
+            long dateTimeMillis;
+            double high;
+            double low;
+            String description;
+
+            JSONObject dayForecast = weatherArray.getJSONObject(i);
+
+            dateTimeMillis = startDay + DateUtil.DAY_MS * i;
+            date = DateUtil.getFriendlyDateString(context, dateTimeMillis, false);
+
+            JSONObject weatherObject =
+                    dayForecast.getJSONArray(OWM_WEATHER).getJSONObject(0);
+
+            description = weatherObject.getString(OWM_DESCRIPTION);
+
+            JSONObject temperatureObject = dayForecast.getJSONObject(OWM_TEMPERATURE);
+            high = temperatureObject.getDouble(OWM_MAX);
+            low = temperatureObject.getDouble(OWM_MIN);
+
+            //...
+        }
 
         return parsedWeatherData;
     }
