8329023125092a8374a5283ac686dc60cace09d0
Joe <j.w.ennis@gmail.com>
2017-01-12 10:43:40 -0500

    Initialize list item layout resource for ForecastAdapter

--- app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -1,18 +1,26 @@
 package com.example.android.sunshine;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.ForecastViewHolder>  {
 
     private String[] mWeatherData;
 
 
+    public ForecastAdapter() {
+
+    }
+
+
     @Override
     public ForecastAdapter.ForecastViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        return null;
+        return new ForecastViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.list_item_forecast, parent, false));
     }
 
 
@@ -39,10 +47,14 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
 
     public class ForecastViewHolder extends RecyclerView.ViewHolder {
 
+        public final TextView mWeatherText;
+
 
         public ForecastViewHolder(View itemView) {
 
             super(itemView);
+
+            mWeatherText = (TextView) itemView.findViewById(R.id.weather_data);
         }
     }
 }
