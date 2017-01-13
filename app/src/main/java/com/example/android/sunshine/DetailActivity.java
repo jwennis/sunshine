eda3a19f142b6e15fc164efcb56a513ca547df16
Joe <j.w.ennis@gmail.com>
2017-01-13 12:30:23 -0500

    Retrieve data from Intent extra in DetailActivity

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,9 +1,13 @@
 package com.example.android.sunshine;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class DetailActivity extends AppCompatActivity {
+
+    private String mForecast;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -11,5 +15,23 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_detail);
+
+        if(savedInstanceState != null) {
+
+            // TODO: restore instance state
+
+        } else {
+
+            Intent detailIntent = getIntent();
+
+            if (detailIntent.hasExtra(Intent.EXTRA_TEXT)) {
+
+                mForecast = detailIntent.getStringExtra(Intent.EXTRA_TEXT);
+
+            } else {
+
+                // TODO: throw WtfException
+            }
+        }
     }
 }
