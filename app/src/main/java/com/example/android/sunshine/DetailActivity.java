9cf23c9ba655d05c190fd2fa1bce98313dbd77d9
Joe <j.w.ennis@gmail.com>
2017-01-13 12:32:26 -0500

    Initialize layout for DetailActivity

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -3,11 +3,12 @@ package com.example.android.sunshine;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
 
     private String mForecast;
-
+    private TextView mWeatherData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +16,8 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_detail);
+
+        mWeatherData = (TextView) findViewById(R.id.weather_data);
 
         if(savedInstanceState != null) {
 
