9dd1c76f0c96290b0cdc9d5bc6015c6af4b4b438
Joe <j.w.ennis@gmail.com>
2017-01-14 12:15:53 -0500

    Add Settings option to DetailActivity

--- app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -58,6 +58,26 @@ public class DetailActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch(item.getItemId()) {
+
+            case R.id.action_settings: {
+
+                startActivity(new Intent(this, SettingsActivity.class));
+
+                return true;
+            }
+
+            default: {
+
+                return super.onOptionsItemSelected(item);
+            }
+        }
+    }
+
+
     private Intent createShareForecastIntent() {
 
         return ShareCompat.IntentBuilder.from(this)
