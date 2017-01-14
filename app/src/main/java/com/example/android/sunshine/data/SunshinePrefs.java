8cb875fbe2f341c60da589f981309628bddabc6c
Joe <j.w.ennis@gmail.com>
2017-01-14 12:46:11 -0500

    Add actual functionality to SunshinePrefs
    Previously was just returning default values

--- app/src/main/java/com/example/android/sunshine/data/SunshinePrefs.java
+++ app/src/main/java/com/example/android/sunshine/data/SunshinePrefs.java
@@ -1,6 +1,10 @@
 package com.example.android.sunshine.data;
 
 import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.preference.PreferenceManager;
+
+import com.example.android.sunshine.R;
 
 
 public class SunshinePrefs {
@@ -10,12 +14,37 @@ public class SunshinePrefs {
 
     public static String getPreferredLocation(Context context) {
 
-        return DEFAULT_WEATHER_LOCATION;
+        SharedPreferences prefs = PreferenceManager
+                .getDefaultSharedPreferences(context);
+
+        String keyForLocation = context.getString(R.string.pref_location_key);
+        String defaultLocation = context.getString(R.string.pref_location_default);
+
+        return prefs.getString(keyForLocation, defaultLocation);
     }
 
 
     public static boolean isMetric(Context context) {
 
-        return true;
+        SharedPreferences prefs = PreferenceManager
+                .getDefaultSharedPreferences(context);
+
+        String keyForUnits = context.getString(R.string.pref_units_key);
+        String defaultUnits = context.getString(R.string.pref_units_metric);
+        String preferredUnits = prefs.getString(keyForUnits, defaultUnits);
+        String metric = context.getString(R.string.pref_units_metric);
+
+        boolean userPrefersMetric;
+
+        if (metric.equals(preferredUnits)) {
+
+            userPrefersMetric = true;
+
+        } else {
+
+            userPrefersMetric = false;
+        }
+
+        return userPrefersMetric;
     }
 }
